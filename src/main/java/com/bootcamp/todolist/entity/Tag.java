0fd9d3585b2cfb08d0da680b066eb27788c122a3
Hins Chiu <chiuhi2@oocl.com>
2020-12-10 19:00:23 +0800

    [toDoList][HinsChiu]feat:init entity

---
+++ src/main/java/com/bootcamp/todolist/entity/Tag.java
@@ -0,0 +1,39 @@
+package com.bootcamp.todolist.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
+public class Tag {
+    @MongoId(FieldType.OBJECT_ID)
+    private String id;
+    private String message;
+    private String color;
+
+    public Tag() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/com/bootcamp/todolist/entity/ToDo.java
@@ -0,0 +1,50 @@
+package com.bootcamp.todolist.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.Set;
+
+@Document
+public class ToDo {
+    @MongoId(FieldType.OBJECT_ID)
+    private String id;
+    private String message;
+    private boolean done;
+    private Set<String> tagIds;
+
+    public ToDo() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Set<String> getTagIds() {
+        return tagIds;
+    }
+
+    public void setTagIds(Set<String> tagIds) {
+        this.tagIds = tagIds;
+    }
+}
